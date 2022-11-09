7ca35837b20714081a6dc139b94a338d3933eb99
Jared Kue <97067837+jbkue@users.noreply.github.com>
2022-11-09 16:50:20 -0500

    main.py has blink code written.
    main.py will make the on board LED blink.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+# main.py -- put your code here!
+from machine import Pin, Timer
+led = Pin(25, Pin.OUT)
+timer = Timer()
+
+def blink(timer):
+    led.toggle()
+
+timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
